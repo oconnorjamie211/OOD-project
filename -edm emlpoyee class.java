f5eef8a60cd2558d016895d695226fb5748ac398
fahd654 <fuwhad654@gmail.com>
2024-11-26 15:01:23 +0000

    updated employee class without errors
    alot of my methods were copied from get name and i forgot to change the varaible types return types etc in this one ive fixed the code and fleshed it out more adding in more methods

--- -edm emlpoyee class.java
+++ -edm emlpoyee class.java
@@ -2,32 +2,35 @@
 public class Employee {
     private String name;
     private String address;
-    private String phoneNumber;
+    private int phoneNumber;
     private String emailAddress;
-    private String ppsn;
+    private int ppsn;
     private String password;
-    private String id;
-    
-    private String IBAN;
+    private int id;
+    private int IBAN;
     private String jobtitle;
     private int payscale;
+    private double hours=0;
+    private double hourlyrate=0;
+    private double totalpay;
 
 
    
 
     
    //password is defaulted as the ppsn
-    public Person(String name, String address, String phoneNumber, String emailAddress, String id, String ppsn, String IBAN, String jobtitle, int payscale){
+    public Employee(String name, String address, int phoneNumber, String emailAddress, int id, int ppsn, int IBAN, String jobtitle, int payscale, double hourlyrate){
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.emailAddress = emailAddress;
-        this.password = ppsn;
-        this.id = id;
         this.ppsn = ppsn;
+        this.password =  String.valueOf(ppsn);
+        this.id = id;
         this.IBAN = IBAN;
         this.jobtitle = jobtitle;
         this.payscale = payscale;
+        this.hourlyrate = hourlyrate;
     }
 
     public String getName() {
@@ -38,7 +41,7 @@ public class Employee {
         return address;
     }
 
-    public String getPhoneNumber() {
+    public int getPhoneNumber() {
         return phoneNumber;
     }
 
@@ -46,85 +49,85 @@ public class Employee {
         return emailAddress;
     }
 
-    public String password() {
+    public String getPassword() {
         return password;
     }
 
-    public String getid() {
+    public int getId() {
         return id;
     }
 
-    public String getppsn() {
+    public int getPpsn() {
         return ppsn;
     }
 
-    public String getIBAN() {
+    public int getIBAN() {
         return IBAN;
     }
 
-    public String gethours() {
+    public double getHours() {
     return hours;
 
     }
 
-    public double getpayrate() {
-        return hours;
+    public double getTotalPay() {
+       totalpay = hours * hourlyrate;
+    return totalpay;
+       
+    }
+    public double getHourlyRate() {
+        return hourlyrate;
     
        
     }
-    public double gethourlyrate() {
-        return hours;
-    
-       
-    }
-    public String getjobtitle(){
+    public String getJobTitle(){
       return jobtitle;
 
 
 
     }
 
-    public int getpayscale(){
+    public int getPayscale(){
     return payscale;
 
     }
     
-    public void changename(String name){
+    public void changeName(String name){
         this.name = name;
             
             }
 
     
-     public void changeaddress(String address){
+     public void changeAddress(String address){
         this.address = address;
                     
          }
 
 
-     public void changephoneNumber(int phoneNumber){
+     public void changePhoneNumber(int phoneNumber){
         this.phoneNumber = phoneNumber;
                 
             }   
             
             
-    public void changeemailaddress(String emailAddress){
+    public void changeEmailAddress(String emailAddress){
          this.emailAddress = emailAddress;
                     
             }
 
 
 
-    public void changepassword(String password){
+    public void changePassword(String password){
     this.password=password;
     
     }
 
-    public void changeid(int id){
+    public void changeId(int id){
     this.id = id;
         
         }
 
-    public void changeppsn(int ppsn){
+    public void changePpsn(int ppsn){
     this.ppsn=ppsn;
                 
        }
@@ -133,11 +136,17 @@ public class Employee {
     this.IBAN = IBAN;
     }
 
+    public void changeJobTitle(String jobtitle)
+   {this.jobtitle = jobtitle;
+   }
+
+   public void changeHours(double hours){
+    this.hours = hours; 
+    }
 
    
 
-
-
+   
 
 
 
@@ -154,16 +163,23 @@ public class Employee {
 
 
     @Override
-    public String toString(){
-        return "Person "
-                +"\nName: " + name
-                +"\nAddress: " + address
-                +"\nPhone Number: " + phoneNumber
-                +"\nEmail Address: " + emailAddress
-                +"\npassword: " + password
-                +"\nid: " + id
-                +"\nppsn: " + ppsn
-                +"\nIBAN: " + IBAN;
+    public String toString() {
+        return "Employee {"
+                + "\nName: " + name
+                + "\nAddress: " + address
+                + "\nPhone Number: " + phoneNumber
+                + "\nEmail Address: " + emailAddress
+                + "\nPassword: " + password
+                + "\nID: " + id
+                + "\nPPSN: " + ppsn
+                + "\nIBAN: " +  IBAN
+                + "\nJob Title: " + jobtitle
+                + "\nPayscale: " + payscale
+                + "\nHours: " + hours
+                + "\nhourly rate: " + hourlyrate
+                + "\ntotal pay:" + totalpay
+                + "\n}";
+
 
 
 
