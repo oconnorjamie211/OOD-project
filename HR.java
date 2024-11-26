c4a88341c4138e8e85d7d33f7435d1865fe5b0cb
fahd654 <fuwhad654@gmail.com>
2024-11-26 19:18:08 +0000

    updated hr to methods that actually work
    methods call accounts as object and change things accordingly

--- HR.java
+++ HR.java
@@ -1,14 +1,46 @@
-
-public class HR extends Employee
-{
+public class hr {
 
 
-public void changejobtitle(String changejobtitle)
-{this.jobtitle = changejobtitle;
- }
+public void changePayscale(Employee account, int payscale){
+    
+account.changePayscale(payscale);
 
- public int changepayscale(int payscale)
- {this.payscale = payscale;
- }
+}
+
+
+public void changeJobTitle(Employee account, String jobtitle){
+
+account.changeJobTitle(jobtitle);
+
+}
+
+public void changeHours(Employee account, double hours){
+
+account.changeHours(hours);
+
+}
+
+public void changehourlyrate(Employee account, double hourlyrate){
+
+account.changehourlyrate(hourlyrate);
+
+} 
+
+public void changePassword(Employee account, String password){
+
+account.changePassword(password);
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 }
