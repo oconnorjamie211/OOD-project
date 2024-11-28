9f2884c1b1bc069bcdbe7e1b90327a193cbb14ce
hsamps <hsampers77@gmail.com>
2024-11-28 18:02:03 +0000

    more parts are working for the menu
    logins are created now for hr and admin and employee there are 2 supporting csv files necessary ftEmployeeData.csv and ptEmployeeData.csv ft stores full time emps and pt stores part time emps as they have different data fields they can not be saved in the same csv the menu class contains a csv reader that reads both files and places data in a hash map using the name of emps as the key (loadEmployees). there is a writer that saves all of the employees and their data back on to the csvs everytime an emp is created and when the final quit on the first menu it saves(saveEmployees). run method contains code for the actual menu constructor creates hash maps for both types of emps. also inits a scanner for the system, and calls load employees to load all the data into the hash maps. the class payrollsim is used to run the run method in payrollsystem, and creates a new instance of the menu. Let me if you find ways to break it the last step will be adding error prevention. to make it unbreakable as they will try to break the code.

--- ULPayrollSystemMenu.java
+++ ULPayrollSystemMenu.java
@@ -11,15 +11,18 @@ public class ULPayrollSystemMenu {
     private Scanner in;
     //creating a hash map as it will keep username and password linked avoiding errors
     private static Map<String, String> credentials = new HashMap<>();
-    private static final String EMPLOYEE_FILE = "employeeData.csv";
-    private Map<String, Employee> employees;
+    private static final String FTEMPLOYEE_FILE = "ftEmployeeData.csv";
+    private static final String PTEMPLOYEE_FILE = "ptEmployeeData.csv";
+    private Map<String, FullTimeEmployee> ftEmployees;
+    private Map<String, PartTimeEmployee> ptEmployees;
 
 
     /**
      * Constructs a ULPayrollSystemMenu object.
      */
     public ULPayrollSystemMenu() {
-        employees = new HashMap<>();
+        ftEmployees = new HashMap<>();
+        ptEmployees = new HashMap<>();
         loadEmployees();
         in = new Scanner(System.in);
     }
@@ -40,37 +43,86 @@ public class ULPayrollSystemMenu {
             if (input.equals("E")) {
                 boolean empSession = true;
                 while (empSession) {
-                    System.out.print("Employee Login type Q to quit\nEnter your name: ");
-                    String enteredName = in.nextLine().trim();
-                    if (enteredName.equalsIgnoreCase("Q")) {
-                        empSession = false;
-                    } else {
-                        Employee emp = employees.get(enteredName);
-
-                        if (emp != null) {
-                            System.out.println("Login successful! Welcome, " + emp.getName());
-                            boolean empSubSession = true;
-
-                            while (empSubSession) {
-                                System.out.println("P)ersonal Details V)iew Payslips Q)uit");
-                                String choice = in.nextLine().toUpperCase().trim();
-                                if (choice.equals("P")) {
-                                    System.out.println(emp.toString());
-                                } else if (choice.equals("V")) {
-                                    System.out.println("to be implemented");
-                                } else if (choice.equals("Q")) {
-                                    empSubSession = false;
-                                    empSession = false;
-                                }
-
-                            }
+                    System.out.println("Employee Login");
+                    System.out.println("F)ull-Time Employee P)art-Time Employee Q)uit");
+                    input = in.nextLine().toUpperCase();
+                    if(input.equals("P")){
+                        System.out.print("Enter Employee username(name): ");
+                        String enteredName = in.nextLine().trim();
+                        if (enteredName.equalsIgnoreCase("Q")) {
+                            empSession = false;
                         } else {
-                            System.out.println("Employee does not exist. Please try again.");
+                            PartTimeEmployee emp = ptEmployees.get(enteredName);
+
+                            if (emp != null) {
+                                String password = emp.getPassword();
+                                System.out.print("Enter Password(ppsn): ");
+                                input = in.nextLine();
+                                if (password.equals(input)) {
+                                    System.out.println("Login successful! Welcome, " + emp.getName());
+                                    boolean empSubSession = true;
+
+                                    while (empSubSession) {
+                                        System.out.println("P)ersonal Details V)iew Payslips Q)uit");
+                                        String choice = in.nextLine().toUpperCase().trim();
+                                        if (choice.equals("P")) {
+                                            System.out.println(emp.toString());
+                                        } else if (choice.equals("V")) {
+                                            System.out.println("to be implemented");
+                                        } else if (choice.equals("Q")) {
+                                            empSubSession = false;
+                                            empSession = false;
+                                        }
+
+                                    }
+                                }
+                            } else {
+                                System.out.println("Employee does not exist. Please try again.");
+                            }
                         }
+                    }else if(input.equals("F")){
+                        System.out.print("Enter Employee username(name): ");
+                        String enteredName = in.nextLine().trim();
+                        if (enteredName.equalsIgnoreCase("Q")) {
+                            empSession = false;
+                        } else {
+                            FullTimeEmployee emp = ftEmployees.get(enteredName);
+
+                            if (emp != null) {
+                                String password = emp.getPassword();
+                                System.out.print("Enter Password(ppsn): ");
+                                input = in.nextLine();
+                                if (password.equals(input)) {
+                                    System.out.println("Login successful! Welcome, " + emp.getName());
+                                    boolean empSubSession = true;
+
+                                    while (empSubSession) {
+                                        System.out.println("P)ersonal Details V)iew Payslips Q)uit");
+                                        String choice = in.nextLine().toUpperCase().trim();
+                                        if (choice.equals("P")) {
+                                            System.out.println(emp.toString());
+                                        } else if (choice.equals("V")) {
+                                            System.out.println("to be implemented");
+                                        } else if (choice.equals("Q")) {
+                                            empSubSession = false;
+                                            empSession = false;
+                                        }
+
+                                    }
+                                }
+                            } else {
+                                System.out.println("Employee does not exist. Please try again.");
+                            }
+                        }
+                    } else if (input.equals("Q")) {
+                        empSession = false;
                     }
+
+
+
                 }
                 //admin functionality
-            }else if(input.equals("A")) {
+            } else if (input.equals("A")) {
                 boolean adminSession = true;
                 while (adminSession) {
                     String adminPassword = "123456789";
@@ -80,19 +132,22 @@ public class ULPayrollSystemMenu {
 
                     //login is successful it will run the if otherwise runs the else
                     if (adminPassword.equals(input)) {
-                        //runs this if login successful
-                        System.out.println("A)dd Employee Q)uit");
-                        input = in.nextLine().toUpperCase();
-                        if (input.equals("A")) {
-                            adminCreateEmployee(in);
+                        boolean adminSubSession = true;
+                        while (adminSubSession) {
+                            //runs this if login successful
+                            System.out.println("A)dd Employee Q)uit");
+                            input = in.nextLine().toUpperCase();
+                            if (input.equals("A")) {
+                                adminCreateEmployee(in);
 
-                        } else if (input.equals("Q")) {
-                            adminSession = false;
-                        } else {
-                            System.out.println("Invalid input");
+                            } else if (input.equals("Q")) {
+                                adminSubSession = false;
+                            } else {
+                                System.out.println("Invalid input");
+                            }
                         }
 
-                    } else if(input.equalsIgnoreCase("Q")) {
+                    } else if (input.equalsIgnoreCase("Q")) {
                         adminSession = false;
 
                     } else {
@@ -103,7 +158,7 @@ public class ULPayrollSystemMenu {
             //runs if input is h
             else if (input.equals("H")) {
                 boolean hrSession = true;
-                while(hrSession) {
+                while (hrSession) {
                     //loads csv file passwords and usernames
                     //String filePath = "humanResourcesCredentials.csv";
                     //credentialsReader(filePath);
@@ -112,24 +167,27 @@ public class ULPayrollSystemMenu {
                     input = in.nextLine();
                     System.out.println("");
                     if (input.equals(hrPassword)) {
-                        System.out.println("P)romote Employee Q)uit");
-                        input = in.nextLine().toUpperCase();
-                        if (input.equals("P")) {
-                            System.out.println("Waiting on code.");
-                        } else if (input.equals("Q")) {
-                            hrSession = false;
-                        } else {
-                            System.out.println("Invalid input");
+                        boolean hrSubSession = true;
+                        while (hrSubSession) {
+                            System.out.println("P)romote Employee Q)uit");
+                            input = in.nextLine().toUpperCase();
+                            if (input.equals("P")) {
+                                System.out.println("Waiting on code.");
+                            } else if (input.equals("Q")) {
+                                hrSubSession = false;
+                            } else {
+                                System.out.println("Invalid input");
+                            }
                         }
 
-                    }else if(input.equalsIgnoreCase("q")){
+                    } else if (input.equalsIgnoreCase("q")) {
                         hrSession = false;
-                    }else {
+                    } else {
                         System.out.println("Access denied.");
                     }
                 }
             }
-                //quitting session
+            //quitting session
             else if (input.equals("Q")) {
                 //setting session false so it will terminate while loop
                 session = false;
@@ -143,26 +201,16 @@ public class ULPayrollSystemMenu {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-    public void employeeLogin(){
+    public void employeeLogin() {
 
     }
+
     //add a quit funcitnoality
-    public void adminCreateEmployee(Scanner scanner){
+    public void adminCreateEmployee(Scanner scanner) {
         System.out.println("Creating An Employee");
         System.out.println("Is this employee F)ull-Time or P)art-Time");
         String input = scanner.nextLine().toUpperCase();
-        if (input.equals("F") || input.equals("P")){
+        if (input.equals("F") || input.equals("P")) {
             System.out.print("Name: ");
             String name = scanner.nextLine();
             System.out.print("Job Title: ");
@@ -179,16 +227,16 @@ public class ULPayrollSystemMenu {
             String phoneNumber = scanner.nextLine();
             System.out.print("Email: ");
             String emailAddress = scanner.nextLine();
-            if(input.equals("F")){
+            if (input.equals("F")) {
                 System.out.print("Pay Scale: ");
                 String payScale = scanner.nextLine();
                 FullTimeEmployee emp = new FullTimeEmployee(name, jobTitle, ppsn, workId, iban, address, phoneNumber, emailAddress, payScale);
-                employees.put(name, emp);
+                ftEmployees.put(name, emp);
                 saveEmployees();
                 System.out.println("Full-Time Employee created successfully.");
-            } else{
-               PartTimeEmployee emp = new PartTimeEmployee(name, jobTitle, ppsn, workId, iban, address, phoneNumber, emailAddress);
-                employees.put(name, emp);
+            } else {
+                PartTimeEmployee emp = new PartTimeEmployee(name, jobTitle, ppsn, workId, iban, address, phoneNumber, emailAddress);
+                ptEmployees.put(name, emp);
                 saveEmployees();
                 System.out.println("Part-Time Employee created successfully.");
             }
@@ -197,20 +245,25 @@ public class ULPayrollSystemMenu {
     }
 
     public void loadEmployees() {
-
-        try (Scanner scanner = new Scanner(new File(EMPLOYEE_FILE))) {
+        try (Scanner scanner = new Scanner(new File(FTEMPLOYEE_FILE))) {
             if (scanner.hasNextLine()) {
                 scanner.nextLine();
             }
             while (scanner.hasNextLine()) {
                 String[] data = scanner.nextLine().split(",");
-                if (data.length > 8){
-                    employees.put(data[0], new FullTimeEmployee(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]));
-                } else {
-                    //changing the first data point chnages how emplyees are found
-                    employees.put(data[0], new PartTimeEmployee(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]));
-                }
+                ftEmployees.put(data[0], new FullTimeEmployee(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]));
+            }
+        } catch (IOException e) {
+            System.out.println("No existing employee data found.");
+        }
 
+        try (Scanner scanner2 = new Scanner(new File(PTEMPLOYEE_FILE))) {
+            if (scanner2.hasNextLine()) {
+                scanner2.nextLine();
+            }
+            while (scanner2.hasNextLine()) {
+                String[] data2 = scanner2.nextLine().split(",");
+                ptEmployees.put(data2[0], new PartTimeEmployee(data2[0], data2[1], data2[2], data2[3], data2[4], data2[5], data2[6], data2[7]));
             }
         } catch (IOException e) {
             System.out.println("No existing employee data found.");
@@ -218,22 +271,30 @@ public class ULPayrollSystemMenu {
     }
 
     public void saveEmployees() {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(EMPLOYEE_FILE))) {
-            writer.println("Name, JobTitle, Ppsn, Password, Id, IBAN, Address, Phone Number, Email, PayScale");
-            for (Employee emp : employees.values()) {
-                if(emp instanceof FullTimeEmployee){
-                    writer.println(String.join(",", emp.getName(), emp.getJobTitle(), ""+emp.getPpsn(), emp.getPassword(),
-                            ""+emp.getId(), ""+emp.getIban(), emp.getAddress(), emp.getPhoneNumber(), emp.getEmailAddress(),  ((FullTimeEmployee) emp).getPayScale()));
-                } else if (emp instanceof PartTimeEmployee) {
-                    writer.println(String.join(",", emp.getName(), emp.getJobTitle(), ""+emp.getPpsn(), emp.getPassword(),
-                            ""+emp.getId(), ""+emp.getIban(), emp.getAddress(), emp.getPhoneNumber(), emp.getEmailAddress()));
-                }
-
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FTEMPLOYEE_FILE))) {
+            writer.println("Name, JobTitle, Ppsn, Password, Id, IBAN, Address, Phone Number, Email, Pay Scale");
+            for (FullTimeEmployee emp : ftEmployees.values()) {
+                writer.println(String.join(",", emp.getName(), emp.getJobTitle(), "" + emp.getPpsn(), emp.getPassword(),
+                        "" + emp.getId(), "" + emp.getIban(), emp.getAddress(), emp.getPhoneNumber(), emp.getEmailAddress(), emp.getPayScale()));
             }
         } catch (IOException e) {
-            System.out.println("Failed to save employee data.");
+            System.out.println("Failed to save Full-Time Employee Data.");
+        }
+
+
+        try (PrintWriter writer2 = new PrintWriter(new FileWriter(PTEMPLOYEE_FILE))) {
+            writer2.println("Name, JobTitle, Ppsn, Password, Id, IBAN, Address, Phone Number, Email");
+            for (PartTimeEmployee emp : ptEmployees.values()) {
+                writer2.println(String.join(",", emp.getName(), emp.getJobTitle(), "" + emp.getPpsn(), emp.getPassword(),
+                        "" + emp.getId(), "" + emp.getIban(), emp.getAddress(), emp.getPhoneNumber(), emp.getEmailAddress()));
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to save Part-Time Employee Data.");
         }
     }
+
+}
+
 
 
 /*
@@ -308,4 +369,4 @@ public class ULPayrollSystemMenu {
             }
         }
     }*/
-}
+
