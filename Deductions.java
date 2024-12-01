d05a32f00b118313d71f35aba2dcf3ce762cbd94
oconnorjamie211 <23354879@studentmail.ul.ie>
2024-12-01 00:45:34 +0000

    Fully updated deductions class

--- Deductions.java
+++ Deductions.java
@@ -1,27 +1,38 @@
-
+import java.time.Month;
+import java.time.LocalDate;
 public class Deductions {
+    private double grossSalary;
     private double grossPay;
     private double IncomeTax;
     private double PRSI;
     private double USC;
+    private double TotalDeductions;
+    private double NetPay;
     private double HealthInsurance;
     private double UnionFees;
-    private double NetPay;
-    private int PayWeek;
+    private int PayWeeks;
 
-    public Deductions(double grossPay, int PayWeek) {
-        this.grossPay = grossPay;
-        this.PayWeek = PayWeek;
+    public Deductions(Employee employee, int PayWeeks) {
+        this.grossSalary = employee.getgrossSalary();
+        this.PayWeeks = PayWeeksCalculation();
     }
 
-    public double getHealthInsurance(){
-        HealthInsurance = 40;
-        return HealthInsurance;
+    public int PayWeeksCalculation() {
+        Month currentMonth = LocalDate.now().getMonth();
+        if(currentMonth == Month.JANUARY || currentMonth == Month.APRIL || currentMonth == Month.JULY || currentMonth == Month.OCTOBER){
+            return 5;
+        }else {
+            return 4;
+        }
     }
 
-    public double getUnionFees(){
-        UnionFees = 25;
-        return UnionFees;
+    public double getgrossPay(){
+        if (PayWeeks == 4){
+            grossPay = ((grossSalary/52) * 4);
+        }else if (PayWeeks == 5){
+            grossPay = ((grossSalary/52) * 5);
+        }
+        return grossPay;
     }
 
     public double getIncomeTax() {
@@ -34,15 +45,15 @@ public class Deductions {
     }
 
     public double getPRSI (){
-        if (PayWeek == 4) {
-            if (grossPay / 4 <= 352) {
+        if (PayWeeks == 4) {
+            if (grossPay/4 <= 352) {
                 PRSI = 0;
             } else if (grossPay / 4 >= 352.01) {
                 PRSI = 0.041;
             }
         }
-        else if (PayWeek == 5) {
-            if (grossPay / 5 <= 352){
+        else if (PayWeeks == 5) {
+            if (grossPay/5 <= 352){
                 PRSI = 0;
             }
             else if (grossPay / 4 >= 352.01){
@@ -66,9 +77,37 @@ public class Deductions {
         return USC;
     }
 
+    public double getHealthInsurance() {
+        HealthInsurance = 40;
+        return HealthInsurance;
+    }
+
+    public double getUnionFees() {
+        UnionFees = 25;
+        return UnionFees;
+    }
+
+    public double getTotalDeductions() {
+        TotalDeductions = IncomeTax + PRSI + USC + HealthInsurance + UnionFees;
+        return TotalDeductions;
+    }
+
     public double getNetPay() {
         NetPay = grossPay - (IncomeTax + PRSI + USC + HealthInsurance + UnionFees);
         return NetPay;
+    }
+
+    public String toString() {
+        return "Deductions: "
+                +"\nGross Salary:" + grossSalary
+                +"\nIncome Tax:" + IncomeTax
+                +"\nPRSI:" + PRSI
+                +"\nUSC:" + USC
+                +"\nHealth Insurance:" + HealthInsurance
+                +"\nUnion Fees:" + UnionFees
+                +"\nTotal Deductions:" + TotalDeductions
+                +"\nNet Pay:" + NetPay;
+
     }
 
 
